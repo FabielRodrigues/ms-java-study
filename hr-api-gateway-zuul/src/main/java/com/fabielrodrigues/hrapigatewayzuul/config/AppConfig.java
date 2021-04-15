1c24028382e0e15626804aabf7fd2a5635649b9a
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2021-04-15 08:28:45 -0300

    - Subindo os arquivos que faltaram do último commit.

--- hr-api-gateway-zuul/src/main/java/com/fabielrodrigues/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/fabielrodrigues/hrapigatewayzuul/config/AppConfig.java
@@ -1,18 +1,10 @@
-package com.fabielrodrigues.hroauth.config;
+package com.fabielrodrigues.hrapigatewayzuul.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@Configuration
 public class AppConfig {
-
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
@@ -25,4 +17,5 @@ public class AppConfig {
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
+
 }
